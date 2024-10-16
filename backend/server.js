12b1ea6b172bb2bc8ed5ccb6903f86f205cc2da0
Julio Angelo R Lim <jrlim@ccc.edu.ph>
2024-10-16 12:20:05 +0800

    changes backend

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,13 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error.message);
 });
-
+//test
+app.get('/', (req, res) => res.status(200).json({ message:'MIRAMONTE'}))
 
 // USER ROUTES
 app.use('/api/users', userRoutes);
